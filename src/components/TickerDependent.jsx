86693792ec08bc7bc3b78c5d002bef70c015e4c9
AlbertMundadan <albert.mundadan+github@gmail.com>
2024-08-05 16:42:51 -0400

    Functional Deployment

--- src/components/TickerDependent.jsx
+++ src/components/TickerDependent.jsx
@@ -3,6 +3,7 @@ import RightStockBar from "./RightStockBar";
 import { StockInfoCall } from "../API/StockInfoCall";
 import React, { useEffect, useState } from "react";
 import Loading from "./Loading";
+import { Link } from "react-router-dom";
 
 
 
@@ -87,15 +88,22 @@ function TickerDependent({ticker}) {
         <>
         <div className="min-w-92 w-4/5 max-w-full h-184 mx-3 mt-4">
           <div className="backdrop-blur-3xl bg-gray-950/40 w-full h-100 rounded-2xl shadow-lg border-slate-900 border-2 mb-3">
-            <div className="text-white text-3xl font-francois justify-center flex pt-10">
+            <div className="text-white text-3xl font-francois flex flex-col justify-center items-center pt-10">
             <svg xmlns="http://www.w3.org/2000/svg" fill="none" viewBox="0 0 24 24" stroke-width="1.5" stroke="currentColor" className="size-9 mr-3">
               <path stroke-linecap="round" stroke-linejoin="round" d="M12 9v3.75m-9.303 3.376c-.866 1.5.217 3.374 1.948 3.374h14.71c1.73 0 2.813-1.874 1.948-3.374L13.949 3.378c-.866-1.5-3.032-1.5-3.898 0L2.697 16.126ZM12 15.75h.007v.008H12v-.008Z" />
             </svg>
 
               Info Not Found. Please Verify Ticker. 
 
+            
+            <Link
+              to="/"
+              className="text-white bg-gray-700 hover:bg-gray-800 text-lg rounded-md px-3 py-2 mt-4 ">
+              Go Back
+            </Link>
             </div>
           </div>
+         
           <div className="backdrop-blur-3xl bg-gray-950/20 min-w-96 h-65 rounded-2xl shadow-lg border-slate-900 border-2 px-1">
           </div>
         </div>
